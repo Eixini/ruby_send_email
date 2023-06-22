6d6df22047c508a6e088ce00f983765fa282d72b
Eixini <eixi16@gmail.com>
2023-06-22 17:31:49 +0300

    Add exception handling

--- send_email.rb
+++ send_email.rb
@@ -4,11 +4,10 @@ require "io/console"
 puts "Enter your email:"
 my_mail = STDIN.gets.chomp
 
-
 puts "Enter your email password to send email:"
 password = STDIN.noecho(&:gets).chomp
 
-puts "Enter mail provider (gmail, yandex, mail, rambler ...):"
+puts "Enter mail provider (gmail.com, yandex.ru, mail.ru, rambler.ru ...):"
 email_provider = STDIN.gets.chomp
 
 puts "Email recipient:"
@@ -20,22 +19,31 @@ subject = STDIN.gets.chomp
 puts "What to write in a letter:"
 body = STDIN.gets.chomp
 
-Pony.mail(
-{
-    :subject => subject,
-    :body => body,
-    :to => send_to,
-    :from => my_mail,
+begin
+    Pony.mail(
+    {
+        :subject => subject,
+        :body => body,
+        :to => send_to,
+        :from => my_mail,
 
-    :via => :smtp,
-    :via_options => {
-        :address => "smtp.#{email_provider}.com",
-        :port => '465',
-        :tls => true,
-        :user_name => my_mail,
-        :password => password,
-        :authentication => :plain
-    }
-})
+        :via => :smtp,
+        :via_options => {
+            :address => "smtp.#{email_provider}",
+            :port => '465',
+            :tls => true,
+            :user_name => my_mail,
+            :password => password,
+            :authentication => :plain
+        }
+    })
+    puts "Email sent successfully!"
 
-puts "Email sent successfully!"
+rescue SocketError => error
+    puts error.message
+rescue Net::SMTPSyntaxError => error
+    puts error.message
+rescue Net::SMTPAuthenticationError => error
+    puts "Incorrect password."
+    puts error.message
+end
